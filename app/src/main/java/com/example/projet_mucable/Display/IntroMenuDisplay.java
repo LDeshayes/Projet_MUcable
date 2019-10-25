f965842a8a7e635659c4b0bcd2e873b950de4e5e
vlion72 <valentin.lion@gmail.com>
2019-10-25 14:16:26 +0200

    push du java

--- app/src/main/java/com/example/projet_mucable/Display/IntroMenuDisplay.java
+++ app/src/main/java/com/example/projet_mucable/Display/IntroMenuDisplay.java
@@ -2,5 +2,36 @@ package com.example.projet_mucable.Display;
 
 // Menu principal de l'application, 4.1 cdc
 
-public class IntroMenuDisplay {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.projet_mucable.Cahier;
+import com.example.projet_mucable.R;
+
+public class IntroMenuDisplay extends AppCompatActivity {
+
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intromenu_display);
+
+
+        Button btn = (Button) findViewById(R.id.buttonVoca);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IntroMenuDisplay.this, CahierDisplay.class));
+            }
+        });
+
+    }
+
+
 }
