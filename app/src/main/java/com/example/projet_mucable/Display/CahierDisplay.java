ffd6bcc8fa8f756ede378b4ece59a5ee79c6a68f
Leonhard <leonhard.deshayes@gmail.com>
2019-10-25 14:08:47 +0200

    Starting choixlangue & cahier

--- app/src/main/java/com/example/projet_mucable/Display/CahierDisplay.java
+++ app/src/main/java/com/example/projet_mucable/Display/CahierDisplay.java
@@ -2,5 +2,18 @@ package com.example.projet_mucable.Display;
 
 // Affiche une liste de vocabulaire d'une langue choisie, 4.2
 
-public class CahierDisplay {
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.projet_mucable.R;
+
+public class CahierDisplay extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_cahier_display);
+    }
+
 }
--- app/src/main/java/com/example/projet_mucable/Display/ChoixLangueDisplay.java
+++ app/src/main/java/com/example/projet_mucable/Display/ChoixLangueDisplay.java
@@ -2,5 +2,26 @@ package com.example.projet_mucable.Display;
 
 // Activité entre menu pcp et cahier pour choisir la langue du cahier
 
-public class ChoixLangueDisplay {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.projet_mucable.R;
+
+public class ChoixLangueDisplay extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setupElements();
+
+        setContentView(R.layout.activity_choixlangue_display);
+    }
+
+    public void ActivityToCahierDisplay(View view) {
+    }
+
+    void setupElements() {
+    }
 }
