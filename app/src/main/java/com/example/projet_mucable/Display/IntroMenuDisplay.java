40c5aaca1e94ec743050c554ad3de49ab7ea66d0
Leonhard <leonhard.deshayes@gmail.com>
2019-10-25 14:36:50 +0200

    Trucs qui marchent

--- app/src/main/java/com/example/projet_mucable/Display/IntroMenuDisplay.java
+++ app/src/main/java/com/example/projet_mucable/Display/IntroMenuDisplay.java
@@ -14,20 +14,17 @@ import com.example.projet_mucable.R;
 
 public class IntroMenuDisplay extends AppCompatActivity {
 
-
-
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intromenu_display);
-
 
         Button btn = (Button) findViewById(R.id.buttonVoca);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(IntroMenuDisplay.this, CahierDisplay.class));
+                startActivity(new Intent(IntroMenuDisplay.this, ChoixLangueDisplay.class));
             }
         });
 
