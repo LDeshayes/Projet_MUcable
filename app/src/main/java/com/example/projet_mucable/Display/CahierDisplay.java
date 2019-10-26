5ea224a6a178da80b147a22123bd03073109b2bd
Leonhard <leonhard.deshayes@gmail.com>
2019-10-26 11:52:15 +0200

    onItemClic working from the listview, set the key of the item clicked

--- app/src/main/java/com/example/projet_mucable/Display/CahierDisplay.java
+++ app/src/main/java/com/example/projet_mucable/Display/CahierDisplay.java
@@ -5,8 +5,11 @@ package com.example.projet_mucable.Display;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.projet_mucable.CustomAdapter;
 import com.example.projet_mucable.R;
@@ -16,7 +19,8 @@ public class CahierDisplay extends Activity {
     ListView words_listview;
     String words_list[] = { "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "lo" };
     String translations_list[] = { "Un", "Deux", "Trois", "Quatre", "Cinq", "Six", "Sept", "Huit", "Neuf", "Dix", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "lo" };
-    String tags_list[] = { "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "lo" };
+    String tags_list[] = { "Nombre", "Nombre", "Nombre", "Nombre", "Nombre", "Nombre", "Nombre", "Nombre", "Nombre", "Nombre", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "lo" };
+    int key; // future key pour repérer le word à modifier
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +47,20 @@ public class CahierDisplay extends Activity {
         words_listview = (ListView) findViewById(R.id.words_listview);
         CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), words_list, translations_list, tags_list);
         words_listview.setAdapter(customAdapter);
+
+        words_listview = (ListView)findViewById(R.id.words_listview);
+
+        words_listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View v, int pos, long id) {
+
+                TextView textView = (TextView) v.findViewById(R.id.word);
+                key = Integer.parseInt( ( (String) textView.getText() ).split(". ")[0] ) - 1;
+                //Toast.makeText(CahierDisplay.this, "You pressed the item : "+key, Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 
 }
