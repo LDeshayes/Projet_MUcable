d11bd2e449a53a1c94b79420186466c1eb81a54a
Leonhard <leonhard.deshayes@gmail.com>
2019-10-27 15:51:20 +0100

    DB can create and load on first launch

--- app/src/main/java/com/example/projet_mucable/Display/CahierDisplay.java
+++ app/src/main/java/com/example/projet_mucable/Display/CahierDisplay.java
@@ -2,32 +2,57 @@ package com.example.projet_mucable.Display;
 
 // Affiche une liste de vocabulaire d'une langue choisie, 4.2
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.projet_mucable.CustomAdapter;
 import com.example.projet_mucable.R;
 
 public class CahierDisplay extends Activity {
 
+    String language;
     ListView words_listview;
     String words_list[] = { "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "lo" };
     String translations_list[] = { "Un", "Deux", "Trois", "Quatre", "Cinq", "Six", "Sept", "Huit", "Neuf", "Dix", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "lo" };
     String tags_list[] = { "Nombre", "Nombre", "Nombre", "Nombre", "Nombre", "Nombre", "Nombre", "Nombre", "Nombre", "Nombre", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "la", "lo" };
     int key; // future key pour repérer le word à modifier
+    SQLiteDatabase CDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cahier_display);
 
+        getLanguage();
+
+        setupDB();
         setupElements();
+    }
+
+    void getLanguage() {
+        Intent i = getIntent();
+        language = i.getStringExtra("LangueChoisie");
+    }
+
+    void setupDB() {
+        getDB();
+        loadDB();
+    }
+
+    @SuppressLint("WrongConstant")
+    void getDB () {
+        CDB = openOrCreateDatabase("CDB.db", SQLiteDatabase.CREATE_IF_NECESSARY, null );
+    }
+
+    void loadDB() {
+
     }
 
     void setupElements() {
@@ -36,9 +61,6 @@ public class CahierDisplay extends Activity {
     }
 
     void setupLanguage() {
-        Intent i = getIntent();
-        String language = i.getStringExtra("LangueChoisie");
-
         TextView textView = (TextView) findViewById(R.id.language);
         textView.setText(language);
     }
--- app/src/main/java/com/example/projet_mucable/Display/ChoixLangueDisplay.java
+++ app/src/main/java/com/example/projet_mucable/Display/ChoixLangueDisplay.java
@@ -2,9 +2,14 @@ package com.example.projet_mucable.Display;
 
 // Activité entre menu pcp et cahier pour choisir la langue du cahier
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
@@ -12,6 +17,16 @@ import android.widget.Spinner;
 import com.example.projet_mucable.R;
 
 public class ChoixLangueDisplay extends Activity {
+
+    String data_anglais[];
+    String data_allemand[];
+    String data_espagnol[];
+
+    {
+        data_anglais = new String[]{"'One', 'Un', 'Nombre', 'NAN', 'NAN', 'NAN'", "'Two', 'Deux', 'Nombre', 'NAN', 'NAN', 'NAN'", "'Three', 'Trois', 'Nombre', 'NAN', 'NAN', 'NAN'"};
+        data_allemand = new String[]{"'Ein', 'Un', 'Nombre', 'NAN', 'NAN', 'NAN'", "'Zwei', 'Deux', 'Nombre', 'NAN', 'NAN', 'NAN'", "'Drei', 'Trois', 'Nombre', 'NAN', 'NAN', 'NAN'"};
+        data_espagnol = new String[]{"'Una', 'Un', 'Nombre', 'NAN', 'NAN', 'NAN'", "'Dos', 'Deux', 'Nombre', 'NAN', 'NAN', 'NAN'", "'Tres', 'Trois', 'Nombre', 'NAN', 'NAN', 'NAN'"};
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +51,91 @@ public class ChoixLangueDisplay extends Activity {
         Spinner mySpinner = (Spinner) findViewById(R.id.language_spinner);
         String language = mySpinner.getSelectedItem().toString();
 
+        createAndLoadDB();
+
         Intent i = new Intent ( this, CahierDisplay.class );
         i.putExtra( "LangueChoisie", language );
         startActivity( i );
         finish();
 
     }
+
+    @SuppressLint("WrongConstant")
+    void createAndLoadDB() {
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean exist = preferences.getBoolean("FST_LAUNCH", false);
+        if (!exist) {
+
+            // CREATION OF DB
+            SQLiteDatabase CDB = openOrCreateDatabase(
+                    "CDB.db"
+                    , SQLiteDatabase.CREATE_IF_NECESSARY
+                    , null
+            );
+
+            final String Create_table_ANGLAIS =
+                    "CREATE TABLE t_ANGLAIS ("
+                            + "Id_Word INTEGER PRIMARY KEY AUTOINCREMENT,"
+                            + "Word TEXT,"
+                            + "Translation TEXT,"
+                            + "Tag_1 TEXT,"
+                            + "Tag_2 TEXT,"
+                            + "Tag_3 TEXT,"
+                            + "Tag_4 TEXT);";
+
+            final String Create_table_ALLEMAND =
+                    "CREATE TABLE t_ALLEMAND ("
+                            + "Id_Word INTEGER PRIMARY KEY AUTOINCREMENT,"
+                            + "Word TEXT,"
+                            + "Translation TEXT,"
+                            + "Tag_1 TEXT,"
+                            + "Tag_2 TEXT,"
+                            + "Tag_3 TEXT,"
+                            + "Tag_4 TEXT);";
+
+            final String Create_table_ESPAGNOL =
+                    "CREATE TABLE t_ESPAGNOL ("
+                            + "Id_Word INTEGER PRIMARY KEY AUTOINCREMENT,"
+                            + "Word TEXT,"
+                            + "Translation TEXT,"
+                            + "Tag_1 TEXT,"
+                            + "Tag_2 TEXT,"
+                            + "Tag_3 TEXT,"
+                            + "Tag_4 TEXT);";
+
+            CDB.execSQL(Create_table_ANGLAIS);
+            CDB.execSQL(Create_table_ALLEMAND);
+            CDB.execSQL(Create_table_ESPAGNOL);
+
+            // FIRST LOAD OF DB
+            int incr;
+            String Insert_Data;
+            for ( incr = 0; incr < data_anglais.length; incr++ ) {
+                Insert_Data = "INSERT INTO t_ANGLAIS VALUES (NULL,"+data_anglais[incr]+")";
+                CDB.execSQL(Insert_Data);
+            }
+            for ( incr = 0; incr < data_allemand.length; incr++ ) {
+                Insert_Data = "INSERT INTO t_ALLEMAND VALUES (NULL,"+data_allemand[incr]+")";
+                CDB.execSQL(Insert_Data);
+            }
+            for ( incr = 0; incr < data_espagnol.length; incr++ ) {
+                Insert_Data = "INSERT INTO t_ESPAGNOL VALUES (NULL,"+data_espagnol[incr]+")";
+                CDB.execSQL(Insert_Data);
+            }
+
+            // CLOSE THE DB BETWEEN ACTIVITIES ?
+
+            SharedPreferences.Editor DB_EXIST_EDIT = preferences.edit();
+            DB_EXIST_EDIT.putBoolean("FST_LAUNCH", true);
+            DB_EXIST_EDIT.commit();
+
+            Log.i("ChoixLangueDisplay", "DB HAS BEEN CREATED");
+
+        }
+
+        Log.i("ChoixLangueDisplay", "DB HAS BEEN TESTED");
+
+    }
+
 }
