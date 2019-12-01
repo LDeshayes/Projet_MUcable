f318fad2f17482487ad732cbefc1586fa8d07568
vlion72 <valentin.lion@gmail.com>
2019-12-01 11:24:41 +0100

    Revision ihm

---
+++ app/src/main/java/com/example/projet_mucable/Display/RevisionCheckDisplay.java
@@ -0,0 +1,14 @@
+package com.example.projet_mucable.Display;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.projet_mucable.R;
+import android.os.Bundle;
+
+public class RevisionCheckDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_revision_check_display);
+    }
+}
