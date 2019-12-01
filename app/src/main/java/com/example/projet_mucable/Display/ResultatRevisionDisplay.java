f2ef05fe77b2f40d0eb18f09d081e86658f62be7
vlion72 <valentin.lion@gmail.com>
2019-12-01 11:24:41 +0100

    Revision ihm

---
+++ app/src/main/java/com/example/projet_mucable/Display/ResultatRevisionDisplay.java
@@ -0,0 +1,14 @@
+package com.example.projet_mucable.Display;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.projet_mucable.R;
+import android.os.Bundle;
+
+public class ResultatRevisionDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultat_revision_display);
+    }
+}
